29e10316dc703a434ec514d5635a1fbcddddb895
Oliver Tessmer <oliver.tessmer@gmail.com>
2019-05-15 18:53:43 -0400

    added travis config and simple test

---
+++ IDEASpecPy/test.py
@@ -0,0 +1,13 @@
+# Test the functions in IDEASpecPy
+# These tests are designed to emulate the way the module is used in the example notebook
+
+
+# "install" the module and import items
+
+folder_that_contains_code="./IDEASpecPy/"
+
+sys.path.append(os.path.abspath(folder_that_contains_code)) #AK_ECS_multiwavelength_DMK_3 for fast_NPQ_3e-Copy2.py"))
+
+import IDEASpecPy as Ipy
+
+print("sucess!")
